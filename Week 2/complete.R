cfe4cb719ebf040d0a37f59e3ad548d3fb23be46
erjicles <erjicles@gmail.com>
2017-09-21 14:54:51 -0500

    Finished complete.R, added data set to repo

--- Week 2/complete.R
+++ Week 2/complete.R
@@ -1,5 +1,13 @@
 # Week 2 Programming Assignment
 # Part 2
 complete <- function(directory, id = 1:332) {
-    
+    resultData <- data.frame("id" = id, "nobs" = vector(mode = "numeric", length = length(id)))
+    for (i in id) {
+        fileName <- sprintf("%03d.csv", i)
+        fileFullyQualifiedName <- file.path(directory, fileName)
+        monitorData <- read.csv(fileFullyQualifiedName)
+        completeMonitorData <- monitorData[complete.cases(monitorData),]
+        resultData$nobs[resultData$id == i] <- nrow(completeMonitorData)
+    }
+    resultData
 }
