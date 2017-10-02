be746ef0717c6dd5c92732feb602f8a55e7ed251
erjicles <erjicles@gmail.com>
2017-10-01 21:52:21 -0500

    Added initial version of course 3 programming assignment run_analysis.R

---
+++ Course3/GettingAndCleaningDataProject/run_analysis.R
@@ -0,0 +1,7 @@
+## Programming Assignment
+test_subject_test <- read.csv("UCI HAR Dataset/test/subject_test.txt", header=F,sep="")
+test_x_test <- read.csv("UCI HAR Dataset/test/X_test.txt",sep="",header=F)
+test_y_test <- read.csv("UCI HAR Dataset/test/y_test.txt", header=F,sep="")
+train_subject_train <- read.csv("UCI HAR Dataset/train/subject_train.txt", header=F,sep="")
+train_x_train <- read.csv("UCI HAR Dataset/train/X_train.txt",sep="",header=F)
+train_y_train <- read.csv("UCI HAR Dataset/train/y_train.txt", header=F,sep="")
